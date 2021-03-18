3411cf55ee4bc58a6874455aee6c865d76254786
VZSultanov <sltnvv@gmail.com>
2021-03-18 19:45:15 +0300

    zarabotalo, я сделал мигрейт

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 
-urlspatterns = [
-    path('', views.post_list, name='post_list')
+
+
+urlpatterns = [
+    path('', views.post_list, name='post_list'),
 ]
